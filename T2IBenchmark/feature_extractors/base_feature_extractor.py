a60a050bcdc6a7a962c7343247de5884dd5b1003
stasstaf <stasstaf2002@gmail.com>
2023-09-08 16:39:18 +0300

    docstrings for FIDStats, BaseFeatureExtractor, BaseImageLoader, ImageDataset

--- T2IBenchmark/feature_extractors/base_feature_extractor.py
+++ T2IBenchmark/feature_extractors/base_feature_extractor.py
@@ -7,11 +7,45 @@ import torch
 
 
 class BaseFeatureExtractor(ABC):
+    """
+    A base class for feature extraction methods.
 
+    This class serves as an interface for feature extraction techniques
+    and should be subclassed for specific implementations, such as InceptionV3FE.
+    """
     @abstractmethod
     def get_preprocess_fn(self) -> Callable[[Image.Image], np.ndarray]:
+        """
+        Get the preprocessing function for the input images.
+
+        This function should be implemented by the subclass and should
+        define the specific preprocessing steps needed for the feature
+        extractor.
+
+        Returns
+        -------
+        Callable[[Image.Image], np.ndarray]
+            The preprocessing function that takes an input PIL.Image.Image and
+            returns a preprocessed numpy array.
+        """
         pass
 
     @abstractmethod
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Perform the forward pass for the feature extractor.
+
+        This function should be implemented by the subclass and should
+        define the forward pass logic for the feature extractor.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            The input tensor to process.
+
+        Returns
+        -------
+        torch.Tensor
+            The output tensor with the extracted features.
+        """
         pass
--- T2IBenchmark/loaders.py
+++ T2IBenchmark/loaders.py
@@ -8,35 +8,121 @@ from T2IBenchmark.utils import IMAGE_EXTENSIONS
 
 
 class BaseImageLoader(ABC):
+    """
+    A base class for custom image loader implementations.
 
+    This class serves as an interface for various image loading techniques
+    and should be subclassed for specific custom implementations.
+    """
     @abstractmethod
     def __len__(self) -> int:
+        """
+       This method should be implemented by the subclass and should return
+       the total number of images in the loader.
+
+       Returns
+       -------
+       int
+           The total number of images or samples in the loader.
+       """
         pass
 
     @abstractmethod
     def __getitem__(self, idx: int):
+        """
+        Retrieve the image at the specified index.
+
+        This method should be implemented by the subclass and should return
+        the image at a given index with any necessary processing
+        (e.g., resizing, normalization) already applied.
+
+        Parameters
+        ----------
+        idx : int
+            The index of the image or sample to retrieve.
+
+        Returns
+        -------
+        Any
+            The image at the specified index, processing as needed by
+            the subclass implementation.
+        """
         pass
 
 
 class ImageDataset(BaseImageLoader, Dataset):
+    """
+    An image dataset loader for managing and preprocessing image data.
 
+    This class inherits from BaseImageLoader and Dataset to provide an
+    interface for loading and manipulating images, including optional
+    preprocessing steps.
+
+    Attributes
+    ----------
+    paths : List[str]
+       A list of file paths to the images.
+    preprocess_fn : Optional[Callable[[Image.Image], Any]], optional
+       An optional preprocessing function that takes a PIL.Image.Image
+       and applies preprocessing steps before returning the processed image.
+    """
     def __init__(
         self,
         paths: List[str],
         preprocess_fn: Optional[Callable[[Image.Image], Any]] = None
     ):
+        """
+        Initialize the ImageDataset with the list of image paths and an optional preprocessing function.
+
+        Parameters
+        ----------
+        paths : List[str]
+            A list of file paths to the images.
+        preprocess_fn : Optional[Callable[[Image.Image], Any]], optional
+            An optional preprocessing function that takes a PIL.Image.Image
+            and applies preprocessing steps before returning the processed image, by default None.
+        """
         self.paths = paths
         self.preprocess_fn = preprocess_fn if preprocess_fn else lambda x: x
 
     def __len__(self) -> int:
+        """
+        Returns the total number of images in the dataset.
+
+        Returns
+        -------
+        int
+            The total number of images in the dataset.
+        """
         return len(self.paths)
 
     def __getitem__(self, idx: int) -> Any:
+        """
+        Retrieve and preprocess the image at the specified index.
+
+        Parameters
+        ----------
+        idx : int
+            The index of the image to retrieve.
+
+        Returns
+        -------
+        Any
+            The preprocessed image at the specified index.
+        """
         image = Image.open(self.paths[idx])
         preproc = self.preprocess_fn(image)
         return preproc
     
     def __str__(self) -> str:
+        """
+        Returns a string representation of the ImageDataset, showing the total number of items.
+
+        Returns
+        -------
+        str
+            The string representation of the ImageDataset.
+        """
         return f"ImageDataset({self.__len__()} items)"
     
     
