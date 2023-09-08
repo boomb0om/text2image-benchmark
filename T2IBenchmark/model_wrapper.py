Fix CLIP score calculation

T2IBenchmark/model_wrapper.py
@@ -4,7 +4,11 @@ import os
 from PIL import Image
 from io import BytesIO
 import torch
-from torch.utils.data import Dataset, default_collate
+from torch.utils.data import Dataset
+try:
+    from torch.utils.data import default_collate
+except ImportError:
+    from torch.utils.data._utils.collate import default_collate
 
 from T2IBenchmark.loaders import BaseImageLoader
 from T2IBenchmark.utils import IMAGE_EXTENSIONS, set_all_seeds

T2IBenchmark/pipelines.py
@@ -174,7 +174,6 @@ def calculate_clip_score(
 
     score_acc = 0.0
     num_samples = 0.0
-    logit_scale = model.logit_scale.exp()
 
     for image, caption in tqdm(dataloader):
         image_embedding = model.encode_image(image.to(device))
@@ -187,7 +186,7 @@ def calculate_clip_score(
             dim=1, keepdim=True
         ).to(torch.float32)
 
-        score = logit_scale * (image_features * caption_features).sum()
+        score = (image_features * caption_features).sum()
         score_acc += score
         num_samples += image.shape[0]
 